a7fcd5a8985b6980e963f176bd8252427357bd45
ThinkStu <1914493943@qq.com>
2023-10-06 10:11:00 +0800

    robot

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module RateLimiter
+
+go 1.21.1
+
+require github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da
---
+++ robot.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"sync"
+	"time"
+
+	"github.com/golang/groupcache/lru"
+)
+
+type Robot struct {
+	cache     *lru.Cache // 最近最少使用淘汰
+	cacheSize int
+	limiterMu sync.Mutex
+}
+
+func NewRobot(size int) *Robot {
+	return &Robot{
+		cache:     lru.New(size),
+		cacheSize: size,
+	}
+}
+
+func (r *Robot) getOrCreateLimiter(groupID string) *SlidingLimiter {
+	r.limiterMu.Lock()
+	defer r.limiterMu.Unlock()
+	val, exists := r.cache.Get(groupID)
+	if exists {
+		return val.(*SlidingLimiter)
+	}
+	limiter := NewSlidingLimiter(3, 1*time.Second)
+	r.cache.Add(groupID, limiter)
+	return limiter
+}
+
+func (r *Robot) SendMessageToGroup(groupID string, message string) bool {
+	limiter := r.getOrCreateLimiter(groupID)
+	if !limiter.Allow() {
+		return false
+	}
+	// TODO: Add actual logic to send the message to the group
+	return true
+}
+
+func main() {
+	robot := NewRobot(1000) // Assuming a maximum of 1000 active groups
+	groupID := "exampleGroup"
+	message := "Hello, World!"
+	success := robot.SendMessageToGroup(groupID, message)
+	if success {
+		// Message was successfully sent
+	} else {
+		// Rate limit exceeded
+	}
+}
